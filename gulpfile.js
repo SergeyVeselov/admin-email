981c3af37330bc083371fb0d1c24e2b1b7aedfec
Sergey Veselov <weselov.sergey@gmail.com>
2015-06-29 17:21:51 +0300

    Initial commit. Basic layout.

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+var paths = {
+    sass: ['./sass/**/*.scss']
+};
+
+gulp.task('default', ['sass']);
+
+gulp.task('sass', function(done) {
+    gulp.src('./sass/index.scss')
+        .pipe(sass({
+            noCache : true,
+            style   : "compact",
+            errLogToConsole: true
+        }))
+        .pipe(gulp.dest('css'))
+        .on('end', done);
+});
+
+
+gulp.task('watch', ['sass'], function() {
+    gulp.watch(paths.sass, ['sass']);
+});
