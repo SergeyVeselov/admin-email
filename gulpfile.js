2c8adad225be1b6c36670b1ea458b417b05b23c1
Sergey Veselov <weselov.sergey@gmail.com>
2015-07-21 15:30:02 +0300

    Run protractor by 'gulp protractor' command

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var shell = require('gulp-shell');
 
 var paths = {
     sass: ['./sass/**/*.scss']
@@ -7,18 +8,29 @@ var paths = {
 
 gulp.task('default', ['sass']);
 
-gulp.task('sass', function(done) {
+gulp.task('sass', function (done) {
     gulp.src('./sass/index.scss')
         .pipe(sass({
-            noCache : true,
-            style   : "compact",
+            noCache: true,
+            style: "compact",
             errLogToConsole: true
         }))
         .pipe(gulp.dest('public/css'))
         .on('end', done);
 });
 
-
-gulp.task('watch', ['sass'], function() {
+gulp.task('watch', ['sass'], function () {
     gulp.watch(paths.sass, ['sass']);
+});
+
+gulp.task('protractor', function () {
+    var protractor = require("gulp-protractor").protractor;
+    gulp.src(["./tests/*.js"])
+        .pipe(protractor({
+            configFile: "protractor.js",
+            args: ['--baseUrl', 'http://127.0.0.1:4444']
+        }))
+        .on('error', function (e) {
+            throw e
+        })
 });
--- protractor.js
+++ protractor.js
@@ -1,4 +1,4 @@
 exports.config = {
-    seleniumAddress: 'http://127.0.0.1:4444/wd/hub',
+    seleniumServerJar: './node_modules/protractor/selenium/selenium-server-standalone-2.45.0.jar',
     specs: ['tests/mail-list.js']
 };
