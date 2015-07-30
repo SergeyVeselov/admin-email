95a696db28c1047d1d1f533e1446cbc8be355c6d
Sergey Veselov <weselov.sergey@gmail.com>
2015-07-30 19:14:10 +0300

    Message list and full letter paper sheets like design

--- public/js/app.js
+++ public/js/app.js
@@ -1,34 +1,62 @@
 define(['js/lib/backbone', 'js/views/letters', 'js/collections/letters', 'js/views/fullLetter'], function (Backbone, LettersView, Letters, FullLetter) {
     $(function () {
+        const CHECKBOXES_AND_NAME_PREVIEW = 'col-xs-8 col-sm-4';
+        const CHECKBOXES_AND_NAME_FULL_MESSAGE = 'col-lg-8';
+        const MESSAGE_PREVIEW = 'col-xs-12 col-sm-6 col-sm-pull-2 col-lg-7 col-lg-pull-1';
+        const MESSAGE_FULL_MESSAGE = 'col-lg-12';
+        const TIME_PREVIEW = 'col-xs-4 col-sm-2 col-sm-push-6 col-lg-1 col-lg-push-7';
+        const TIME_FULL_MESSAGE = 'col-lg-4';
         var eventBus = {};
         _.extend(eventBus, Backbone.Events);
         var letters = new Letters();
         var lettersView = new LettersView({eventBus: eventBus, collection: letters});
         lettersView.listenTo(letters, 'add', lettersView.addOne);
         Backbone.listenTo(eventBus, 'showLetter', function (model) {
-            $('.mailbox').addClass('container-fluid');
-            $('.mailbox-container').addClass('row');
             var fullLetter = $(".full-letter");
             var fullLetterView = new FullLetter({eventBus: eventBus});
             fullLetter.html(fullLetterView.render(model).el);
             fullLetter.show();
-            fullLetter.addClass('col-xs-8 col-lg-6');
-            $(".letter-preview-container").addClass('col-xs-4 col-lg-6');
-            $(".letter").removeClass("row");
-            $(".checkboxes-and-sender-name").removeClass("col-xs-8 col-sm-4");
-            $(".receiving-time-container").hide();
-            $(".letter-preview").removeClass("col-xs-12 col-sm-6 col-sm-pull-2 col-lg-7 col-lg-pull-1");
-            $('.menu').css({'padding-right': 0});
-            $('.navbar-right-container').css({'padding-right': 0});
+
+            var checkboxes = $('.checkboxes-and-sender-name');
+            checkboxes.removeClass(CHECKBOXES_AND_NAME_PREVIEW);
+            checkboxes.addClass(CHECKBOXES_AND_NAME_FULL_MESSAGE);
+            checkboxes.css({'padding-right': 0});
+
+            var receivingTimeContainer = $('.receiving-time-container');
+            receivingTimeContainer.removeClass(TIME_PREVIEW);
+            receivingTimeContainer.addClass('col-lg-4');
+            receivingTimeContainer.css({'padding-left': 0});
+
+            var letter = $('.letter-preview');
+            letter.removeClass(MESSAGE_PREVIEW);
+            letter.addClass(MESSAGE_FULL_MESSAGE);
+
+            var maillist = $('#mail-list');
+            maillist.css({width: '250px'});
+            maillist.css({float: 'left'});
+
         });
         Backbone.listenTo(eventBus, "closeFullLetter", function() {
             lettersView.render();
-            $('.mailbox').removeClass('container-fluid');
-            $('.mailbox-container').removeClass('row');
-            $(".letter-preview-container").removeClass('col-xs-4 col-lg-6');
             $('.full-letter').hide();
-            $('.menu').css({'padding-right': $('.menu').css('padding-left')});
-            $('.navbar-right-container').css({'padding-right': $('.navbar-right-container').css('padding-left')});
+
+            var checkboxes = $('.checkboxes-and-sender-name');
+            checkboxes.addClass(CHECKBOXES_AND_NAME_PREVIEW);
+            checkboxes.removeClass(CHECKBOXES_AND_NAME_FULL_MESSAGE);
+            checkboxes.css({'padding-right': 0});
+
+            var receivingTimeContainer = $('.receiving-time-container');
+            receivingTimeContainer.addClass(TIME_PREVIEW);
+            receivingTimeContainer.removeClass(TIME_FULL_MESSAGE);
+            receivingTimeContainer.css({'padding-left': 0});
+
+            var letter = $('.letter-preview');
+            letter.addClass(MESSAGE_PREVIEW);
+            letter.removeClass(MESSAGE_FULL_MESSAGE);
+
+            var maillist = $('#mail-list');
+            maillist.css({width: '100%'});
+            maillist.css({float: 'auto'});
         });
         letters.fetch();
     });
